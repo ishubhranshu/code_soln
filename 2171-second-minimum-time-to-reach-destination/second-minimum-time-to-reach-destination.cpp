71c618bee8be4bbf98cbf9a35201ab9b384b471c
ishubhranshu <43900473+ishubhranshu@users.noreply.github.com>
2024-07-28 23:03:41 +0530

    Time: 459 ms (70.22%) | Memory: 206.3 MB (33.50%) - LeetSync

--- 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.cpp
+++ 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.cpp
@@ -1,41 +1,47 @@
 class Solution {
 public:
     int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
-        vector<vector<int>> adj(n + 1);
-        for (auto& edge : edges) {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
+        vector<vector<int>> adj(n+1);
+        for(auto i: edges)
+        {
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
         }
+        vector<int> f(n+1, INT_MAX), s(n+1, INT_MAX);
+        f[1]=0;
 
-        vector<int> f(n + 1, INT_MAX), s(n + 1, INT_MAX);
-        f[1] = 0;
         queue<pair<int, int>> q;
         q.push({1, 0});
 
-        while (!q.empty()) {
-            int cnode = q.front().first;
-            int ctime = q.front().second;
+        while(!q.empty())
+        {
+            int cnode=q.front().first;
+            int ctime=q.front().second;
             q.pop();
 
-            for (int neigh : adj[cnode]) {
-                int ntime = ctime + time;
-                if ((ctime / change) % 2 == 1) {
-                    ntime += change - (ctime % change);
-                }
+            for(auto neigh: adj[cnode])
+            {
+                int ntime=time+ctime;
+                if((ctime/change)%2==1)
+                    ntime+=change-(ctime%change);
 
-                if (ntime < f[neigh]) {
-                    s[neigh] = f[neigh];
-                    f[neigh] = ntime;
-                    q.push({neigh, ntime});
-                } else if (ntime > f[neigh] && ntime < s[neigh]) {
-                    s[neigh] = ntime;
-                    q.push({neigh, ntime});
+                if(ntime<f[neigh])
+                {
+                    s[neigh]=f[neigh];
+                    f[neigh]=ntime;
+                    
+                q.push({neigh, ntime});
                 }
+                else if(ntime<s[neigh] && ntime>f[neigh])
+                {
+                    s[neigh]=ntime;
+                    
+                q.push({neigh, ntime});
+                }
+                if(s[n]!=INT_MAX)
+                    return s[n];
             }
-            
-            if (s[n] != INT_MAX) {
-                return s[n];
-            }
+            // cout<<s.back()<<endl;
         }
 
         return -1;
