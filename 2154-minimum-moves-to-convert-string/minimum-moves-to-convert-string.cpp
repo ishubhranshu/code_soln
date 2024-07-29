5f932612e15c3d12f5549b172023d0658a227f3a
ishubhranshu <43900473+ishubhranshu@users.noreply.github.com>
2024-07-29 12:54:17 +0530

    Time: 4 ms (30.51%) | Memory: 7.9 MB (27.41%) - LeetSync

---
+++ 2154-minimum-moves-to-convert-string/minimum-moves-to-convert-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumMoves(string s) {
+        int ans=0, i=0;
+        while(i<s.length())
+        {
+            if(s[i]=='O')
+                i++;
+            else
+            {
+                ans++;
+                i+=3;
+            }
+        }
+
+        return ans;
+    }
+};
