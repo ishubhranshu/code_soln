cf53f297ec00145f3c8e25458206a26044629aee
ishubhranshu <43900473+ishubhranshu@users.noreply.github.com>
2024-08-08 17:51:36 +0530

    Time: 78 ms (25.92%), Space: 31.8 MB (9.68%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -2,27 +2,48 @@ class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
         int n=points.size();
+        if(n<=2)
+            return n;
         int ans=1;
+        
         for(int i=0; i<n; i++)
         {
-            for(int j=i+1; j<n; j++)
+            unordered_map<string, int> mp;
+            for(int j=0; j<n; j++)
             {
-                int cp=2;
+                if(i==j)
+                    continue;
                 int dy=points[j][1]-points[i][1];
                 int dx=points[j][0]-points[i][0];
-                for(int k=0; k<n; k++)
+
+                if(dx==0)
+                    mp["inf"]++;
+
+                else if(dy/dx<0)
                 {
-                    if(k==i || k==j)
-                        continue;
-                    int dyy=points[k][1]-points[i][1];
-                    int dxx=points[k][0]-points[i][0];
-                    
-                    if(dyy*dx == dxx*dy)
-                        cp++;
+                    dx=abs(dx);
+                    if(dy>0)
+                        dy*=-1;
+                    int gcd=__gcd(abs(dy), abs(dx));
+                    dy/=gcd;
+                    dx/=gcd;
                 }
-                ans=max(ans, cp);
+                else
+                {
+                    int gcd=__gcd(abs(dy), abs(dx));
+                    dy/=gcd;
+                    dx/=gcd;
+                }
+                
+                mp[to_string(dy) + ":" + to_string(dx)]++;
+            }
+            for(auto ii: mp)
+            {   
+                ans=max(ii.second, ans);
+                // cout<<i<<" "<<ii.first<<" "<<ii.second<<endl;
             }
         }
-        return ans;
+        cout<<ans;
+        return ans+1;
     }
 };
