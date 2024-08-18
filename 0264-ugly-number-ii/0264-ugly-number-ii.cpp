537546470b126f9e59d0d6049fb97c3d71ec18bb
ishubhranshu <43900473+ishubhranshu@users.noreply.github.com>
2024-08-18 11:24:39 +0530

    Time: 85 ms (19.64%), Space: 33.9 MB (14.76%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        if (n == 1) return 1;
-        priority_queue<long, vector<long>, greater<long>> pq;
-        unordered_set<long> seen;
-        vector<int> primes = {2, 3, 5};
-        
+        if(n==1)
+            return 1;
+
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
+        unordered_set<long long> st;
+        vector<long long> pf{2,3,5};
         pq.push(1);
-        seen.insert(1);
-        
-        long currentUgly = 1;
-        for (int i = 1; i < n; ++i) {
-            currentUgly = pq.top();
+        st.insert(1);
+        long long count=1;
+
+        while(count<n)
+        {
+            long long cele=pq.top();
             pq.pop();
-            for (int prime : primes) {
-                long nextUgly = currentUgly * prime;
-                if (seen.find(nextUgly) == seen.end()) {
-                    pq.push(nextUgly);
-                    seen.insert(nextUgly);
+            for(auto p: pf)
+            {
+                long long nele=p*cele;
+                if(st.find(nele)==st.end())
+                {
+                    st.insert(nele);
+                    pq.push(nele);
                 }
             }
+            count++;
+            // cout<<count<<" "<<n<<endl;
+            // if(count>10)
+            //     break;
         }
-        
+
         return pq.top();
     }
 };
