7d8b38f46ca45f4a8692baf22fd52b85469b683b
ishubhranshu <43900473+ishubhranshu@users.noreply.github.com>
2024-08-31 07:11:32 +0530

    Time: 121 ms (59.47%), Space: 70.7 MB (30.34%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,45 +1,47 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        
+        vector<vector<pair<int, double>>> adj(n);
 
-        vector<vector<pair<int,double>>> adj(n);
         for(int i=0; i<edges.size(); i++)
         {
-            adj[edges[i][1]].push_back({edges[i][0], succProb[i]});
             adj[edges[i][0]].push_back({edges[i][1], succProb[i]});
+            adj[edges[i][1]].push_back({edges[i][0], succProb[i]});
         }
 
-        vector<double> prob(n, -1);
-        vector<bool> vis(n, 0);
         priority_queue<pair<double, int>> pq;
+        vector<int> vis(n, 0);
+        vector<double> probs(n, 0);
+        
+        pq.push({1, start_node});
+        probs[start_node]=1;
+        
 
-        // vis[start_node]=1;
-        // pq.push({1, start_node});
+        while(!pq.empty())
+        {
+            int cnode=pq.top().second;
+            double cprob=pq.top().first;
+            pq.pop();
+            vis[cnode]=1;
 
-        // while(!pq.empty())
-        // {
-        //     int cnode = pq.top().second;
-        //     double cprob = pq.top().first;
-        //     pq.pop();
+            // cout<<cnode<<": "<<endl;
+            for(auto nodes: adj[cnode])
+            {
+                int neigh=nodes.first;
+                double prob=nodes.second;
+                // cout<<neigh<<" "<<probs[neigh]<<" "<<cprob<<" "<<prob<<endl;
+                if(vis[neigh]==1 || probs[neigh]>=cprob*prob)
+                    continue;
 
-        //     if(cnode  == end_node)
-        //         return cprob;
+                probs[neigh]=cprob*prob;
+                pq.push({probs[neigh], neigh});
+            }
+        }
 
-        //     for(auto node: adj)
-        //     {
-        //         int neigh=node[cnode].first;
-        //         double neighProb=node[cnode].second;
-        //         if(!vis[neigh])
-        //         {
-        //             vis[neigh]=1;
-        //             pq.push({cprob*neighProb, neigh});
-        //         }
-        //     }
-        // }
+        // for(auto i: probs)
+        //     cout<<i<<" ";
+        // cout<<endl;
 
-        return 0;
-
-
+        return probs[end_node];
     }
 };
