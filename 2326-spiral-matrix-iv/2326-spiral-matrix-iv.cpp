e111531478cd8cd883a2c858db8afe3fe972d07e
ishubhranshu <43900473+ishubhranshu@users.noreply.github.com>
2024-09-09 20:52:00 +0530

    Time: 152 ms (87.15%), Space: 130.6 MB (65.45%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
@@ -11,55 +11,53 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
-        vector<vector<int>> mat(m, vector<int>(n, -1));
-
-        int left=0, right=n-1, low=m-1, high=0;
-        int r=0, c=0;
+        vector<vector<int>> ans(m, vector<int>(n, -1));
         ListNode* ptr=head;
-        while(ptr && left<=right && high<=low)
+        n= ans.size();
+        m=ans[0].size();
+        int left=0,up=0,down=n-1,right=m-1;
+        int curr1=left,curr2=up;
+        // vector<int> ans;
+        while(left<=right && up<=down)
         {
-            while(c<=right && ptr)
+            while(ptr && curr2<=right && left<=right && up<=down)
             {
-                // cout<<r<<" "<<c<<" "<<ptr->val<<endl;
-                mat[r][c]=ptr->val;
-                c++;
+                ans[curr1][curr2++]=ptr->val;
                 ptr=ptr->next;
             }
-            high++;
-            r++;
-            c--;
-            while(r<=low && ptr)
+            up++;
+            curr1=up;
+            curr2--;
+
+            while(ptr && curr1<=down && left<=right && up<=down)
             {
-                // cout<<r<<" "<<c<<" "<<ptr->val<<endl;
-                mat[r][c]=ptr->val;
-                r++;
+                ans[curr1++][curr2]=ptr->val;
                 ptr=ptr->next;
             }
             right--;
-            r--;
-            c--;
-            while(c>=left && ptr)
+            curr2=right;
+            curr1--;
+
+            while(ptr && curr2>=left && left<=right && up<=down)
             {
-                // cout<<r<<" "<<c<<" "<<ptr->val<<endl;
-                mat[r][c]=ptr->val;
-                c--;
+                ans[curr1][curr2--]=ptr->val;
                 ptr=ptr->next;
             }
-            low--;
-            r--;
-            c++;
-            while(r>=high && ptr)
+            down--;
+            curr1=down;
+            curr2++;
+
+            while(ptr && curr1>=up && left<=right && up<=down)
             {
-                // cout<<r<<" "<<c<<" "<<ptr->val<<endl;
-                mat[r][c]=ptr->val;
-                r--;
+                ans[curr1--][curr2]=ptr->val;
                 ptr=ptr->next;
             }
             left++;
-            r++;
-            c++;
+            curr2=left;
+            curr1++;
         }
+        return ans;
+        return {};
 
-        return mat; 
     }
 };
