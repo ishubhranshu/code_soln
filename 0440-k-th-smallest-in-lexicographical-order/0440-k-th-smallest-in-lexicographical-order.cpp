be874aaa2996d4c64b5a04e83f649514807b0926
ishubhranshu <43900473+ishubhranshu@users.noreply.github.com>
2024-09-29 17:56:30 +0530

    Time: 2 ms (53.25%), Space: 7.5 MB (50.92%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
-long countSteps(long curr, long n) {
-        long steps = 0, first = curr, last = curr;
-        while (first <= n) {
-            steps += min(n + 1, last + 1) - first;
-            first *= 10;
-            last = last * 10 + 9;
+    long long count(long long left, long long right, long long n)
+    {
+        long long ccount=0;
+        while(left<=n)
+        {
+            ccount+=(right-left);
+            left*=10;
+            right=min(right*10, n+1);
         }
-        return steps;
+        return ccount;
     }
     int findKthNumber(int n, int k) {
-        long curr = 1;  // Start from the first prefix (1)
-        k--;  // Decrease k by 1 because we are starting from 1
-        
-        while (k > 0) {
-            long steps = countSteps(curr, n);
-            if (steps <= k) {
-                curr++;  // Move to the next sibling (prefix)
-                k -= steps;
-            } else {
-                curr *= 10;  // Move to the first child
-                k--;  // We have covered the current number
+        long long left=1;
+        k--;
+        while(k>0)
+        {
+            long long ccount=count(left, left+1, n);
+            if(ccount<=k)
+            {
+                k-=ccount;
+                left++;
+            }
+            else
+            {
+                k-=1;
+                left*=10;
             }
         }
-        
-        return curr;
+
+        return left;
     }
 };
