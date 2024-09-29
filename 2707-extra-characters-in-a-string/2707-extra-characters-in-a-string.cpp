cc72dc537c447ff480cc1e28eb4c542d133e4388
ishubhranshu <43900473+ishubhranshu@users.noreply.github.com>
2024-09-29 18:47:47 +0530

    Time: 109 ms (67.34%), Space: 69 MB (87.05%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    int solve(int ind, string &s, unordered_set<string> &st, int n, vector<vector<int>> &dp)
+    vector<int> dp;
+    int solve(int ind, string &s, unordered_set<string> &st)
     {
         if(ind==s.length())
-            return n;
+            return 0;
 
-        if(dp[ind][n]!=-1)
-            return dp[ind][n];
+        if(dp[ind]!=-1)
+            return dp[ind];
 
         int ans=s.length();
-        for(auto word: st)
+        string cstring="";
+        for(int i=ind; i<s.length(); i++)
         {
-            int len = word.length();
-            // cout<<word<<" "<<s.substr(ind, len)<<endl;
-            if(s.substr(ind, len) == word)
-                ans=min(ans, solve(ind+len, s, st, n-len, dp));
+            int extra=0;
+            cstring+=s[i];
+            if(st.find(cstring) == st.end())
+                extra+=cstring.length();
+            extra+=solve(i+1, s, st);
+            ans=min(ans, extra);
         }
-        ans=min(ans, solve(ind+1, s, st, n, dp));
-        return dp[ind][n]=ans;
+
+        return dp[ind] = ans;
     }
     int minExtraChar(string s, vector<string>& dictionary) {
+        dp.resize(s.length(), -1);
         unordered_set<string> st;
-        for(auto ss: dictionary)
-            st.insert(ss);
-
-        int n=s.length();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        int ans=solve(0, s, st, n, dp);
-
-        for(auto i: dp)
-        {
-            for(auto j: i)
-                cout<<j<<" ";
-            cout<<endl;
-        }
-        return ans;
+        for(auto s: dictionary)
+            st.insert(s);
+        return solve(0, s, st);
     }
 };
