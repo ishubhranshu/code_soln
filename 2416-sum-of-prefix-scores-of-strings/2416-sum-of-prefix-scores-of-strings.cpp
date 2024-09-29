e9f8942b0d9ebfcc32bf223fb96c5c48ba958623
ishubhranshu <43900473+ishubhranshu@users.noreply.github.com>
2024-09-29 20:40:38 +0530

    Time: 1275 ms (6.78%), Space: 726.2 MB (21.19%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.cpp
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.cpp
@@ -1,60 +1,60 @@
+class TrieNode {
+public:
+    unordered_map<char, TrieNode *> child;
+    int count;
+    TrieNode()
+    {
+        count=0;
+    }
+
+};
+class Trie {
+public:
+    TrieNode *root;
+    Trie()
+    {
+        root=new TrieNode();
+    }
+
+    void insert(string s)
+    {
+        TrieNode * node=root;
+        for(auto ch: s)
+        {
+            if(node->child[ch-'a']==NULL)
+                node->child[ch-'a']=new TrieNode();
+            node=node->child[ch-'a'];
+            node->count++;
+        }
+    }
+    int find(string s)
+    {
+        TrieNode * node=root;
+        int count=0;
+        for(auto ch: s)
+        {
+            if(node->child[ch-'a']==NULL)
+                break;
+            node=node->child[ch-'a'];
+            count+=node->count;
+        }
+        return count;
+    }
+};
 class Solution {
 public:
-    struct TrieNode{
-        TrieNode* children[26];
-        int count;
-        
-        TrieNode() {
-            count = 0;
-            for(int i = 0; i < 26; i++) {
-                children[i] = nullptr;
-            }
-        }
-    };
-    
-    TrieNode* getnode() {
-        return new TrieNode();
-    }
-    
-    void insert(TrieNode* root, const string& word) {
-        TrieNode* ptr = root;
-        
-        for(auto c : word) {
-            if(ptr->children[c - 'a'] == nullptr) {
-                ptr->children[c - 'a'] = getnode();
-            }
-            ptr->children[c-'a']->count++;
-            ptr = ptr->children[c - 'a'];
-        }
-    }
-    
-    int getPrefixScore(TrieNode* root, const string& word) {
-        TrieNode* ptr = root;
-        int score = 0;
-        
-        for(auto c : word) {
-            ptr = ptr->children[c - 'a'];
-            score += ptr->count;  // Add the number of words that pass through this prefix
-        }
-        
-        return score;
-    }
-    
     vector<int> sumPrefixScores(vector<string>& words) {
-        TrieNode* root = getnode();
-        
-        // Insert all words into the Trie
-        for(const auto& word : words) {
-            insert(root, word);
+        int n=words.size();
+        Trie tr;
+        for(auto w:words)
+            tr.insert(w);
+
+        vector<int> ans(n);
+        for(int i=0; i<n; i++)
+        {
+            ans[i]=tr.find(words[i]);
         }
-        
-        vector<int> ans(words.size(), 0);
-        
-        // For each word, calculate the sum of scores of all prefixes
-        for(int i = 0; i < words.size(); i++) {
-            ans[i] = getPrefixScore(root, words[i]);
-        }
-        
+
         return ans;
     }
 };
