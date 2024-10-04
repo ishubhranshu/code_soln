7a5e176fd3c29452eb4e7f596be39eed705dd7ac
ishubhranshu <43900473+ishubhranshu@users.noreply.github.com>
2024-10-04 09:56:27 +0530

    Time: 57 ms (84.2%), Space: 55.8 MB (99.6%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
@@ -2,28 +2,34 @@ class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
         int n=skill.size();
-        sort(skill.begin(), skill.end());
-
+        vector<int> freq(1001, 0);
+        
         long long sum=0;
         for(auto i: skill)
-            sum+=i;
-        // if(sum%2)
-        //     return -1;
+        {
+            sum+=i;   
+            freq[i]++;
+        }
 
         int teams=n/2;
         if(sum%teams)
             return -1;
 
         int tsum=sum/teams;
-
-        // cout<<teams<<" "<<tsum<<endl;
-        int left=0, right=n-1;
         long long ans=0;
-        while(left<right)
+        for(int i=0; i<1001; i++)
         {
-            if(skill[left]+skill[right]!=tsum)
-                return -1;
-            ans+=(skill[left++]*skill[right--]);
+            if(freq[i]==0)
+                continue;
+            while(freq[i])
+            {
+                if(freq[tsum-i]==0)
+                    return -1;
+
+                ans+=(i*(tsum-i));
+                freq[i]--;
+                freq[tsum-i]--;
+            }    
         }
         return ans;
     }
