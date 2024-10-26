a7a849b0eca5fef516e57f160635c4e9a53267b3
ishubhranshu <43900473+ishubhranshu@users.noreply.github.com>
2024-10-26 11:22:04 +0530

    Time: 19 ms (95.72%), Space: 45.8 MB (13.7%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int n=people.size();
+        int left=0,right=n-1;
+        int boats=0;
+        while(left<=right && limit<=people[right])
+        {
+            boats++;
+            right--;
+        }
+        // cout<<left<<" "<<right;
+        while(left<right)
+        {
+            boats++;
+            if(people[left]+people[right]<=limit)
+            {
+                left++;right--;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        boats+=right-left+1;
+
+        return boats;
+    }
+};
