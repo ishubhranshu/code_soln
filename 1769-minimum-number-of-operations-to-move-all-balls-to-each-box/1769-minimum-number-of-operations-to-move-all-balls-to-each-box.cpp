b9cfd9dc3cf04fb0f844fa6af0dceb08519a74de
ishubhranshu <43900473+ishubhranshu@users.noreply.github.com>
2025-01-06 15:54:50 +0530

    Time: 4 ms (57.5%), Space: 12.1 MB (50.97%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
         int n=boxes.size();
-        vector<int> left(n, 0), right(n, 0);
-
-        int count= boxes[0]=='1'?1:0;
-        for(int i=1; i<n; i++)
-        {
-            left[i]=count+left[i-1];
-            if(boxes[i]=='1')
-                count++;
-        }
-        count=boxes.back()=='1'?1:0;
-        for(int i=n-2; i>=0; i--)
-        {
-            right[i]=count+right[i+1];
-            if(boxes[i]=='1')
-                count++;
-        }
+        int prefix=0, prefixS=0;
         vector<int> ans(n, 0);
 
         for(int i=0; i<n; i++)
-            ans[i]=left[i]+right[i];
+        {
+            ans[i]+=prefixS;
+            if(boxes[i]=='1')
+                prefix++;
+            prefixS+=prefix;
+        }
+        prefix=0; prefixS=0;
+        for(int i=n-1; i>=0; i--)
+        {
+            ans[i]+=prefixS;
+            if(boxes[i]=='1')
+                prefix++;
+            prefixS+=prefix;
+        }
 
         return ans;
         
