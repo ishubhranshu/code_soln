1326d3ad856c5806ff8d521eb5eb5d3a09016dc9
ishubhranshu <43900473+ishubhranshu@users.noreply.github.com>
2025-02-28 17:35:15 +0530

    Time: 0 ms (100%), Space: 32.3 MB (42.47%) - LeetHub

--- 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.cpp
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.cpp
@@ -21,28 +21,24 @@
  */
 class Solution {
 public:
-    bool dfs(ListNode* head, TreeNode* root)
+    
+    bool checkFurther(ListNode* head, TreeNode* root)
     {
-        if(!root)
-            return 0;
-
-        if((head->val == root->val && dfs2(head, root)))
-            return 1;
-
-        return dfs(head, root->left) || dfs(head, root->right);
-    }
-
-    bool dfs2(ListNode* head, TreeNode* root)
-    {       
         if(!head)
-            return 1;
-        
+            return true;
         if(!root || head->val != root->val)
-            return 0;
+            return false;
 
-        return dfs2(head->next, root->left) || dfs2(head->next, root->right);
+        return checkFurther(head->next, root->left) || checkFurther(head->next, root->right);
     }
     bool isSubPath(ListNode* head, TreeNode* root) {
-        return dfs(head, root);
+        
+        if(!root)
+            return false;
+
+        if(head->val == root->val && checkFurther(head, root))
+            return true;
+
+        return isSubPath(head, root->left) || isSubPath(head, root->right);
     }
 };
