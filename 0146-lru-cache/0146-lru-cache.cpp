5f38c07df2ceb68517be7480935037d28b0640e7
ishubhranshu <43900473+ishubhranshu@users.noreply.github.com>
2025-03-06 11:33:52 +0530

    Time: 124 ms (15.14%), Space: 182.2 MB (34.44%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,99 +1,51 @@
-class DListNode{
-    public:
-    int key;
-    int val;
-    DListNode* next;
-    DListNode* prev;
-    
-
-    DListNode(int x, int y)
-    {
-        key=x;
-        val=y;
-        next=NULL;
-        prev=NULL;
-    }
-    DListNode()
-    {
-        val=0;
-        key=0;
-        next=NULL;
-        prev=NULL;
-    }
-    
-};
 class LRUCache {
-    DListNode* head;
-    DListNode* tail;
-    int maxcap;
-    int currcap;
-    unordered_map<int, DListNode*> ump;
-
 public:
+    list<int> cache;
+    unordered_map<int, pair<list<int>:: iterator, int>> addMap;
+    int n;
     LRUCache(int capacity) {
-        head=new DListNode(-1,-1);
-        tail=new DListNode(-1,-1);
-        
-        head->next=tail;
-        tail->prev=head;
-        maxcap=capacity;
-        currcap=0;
+        n=capacity;
     }
     
+    void makeMostRecent(int key)
+    {
+        auto node=*(addMap[key].first);
+        int val=addMap[key].second;
+        cache.erase(addMap[key].first);
+
+        cache.push_front(key);
+
+        addMap[key]={cache.begin(), val};
+    }
     int get(int key) {
-        if(ump.find(key)==ump.end())
+        if(!addMap.count(key))
             return -1;
-            
-        DListNode* node=ump[key];
-        remove(node);
-        insert(node);
-        return node->val;
+
+        int val=addMap[key].second;
+        makeMostRecent(key);
+
+        return val;
     }
     
     void put(int key, int value) {
-        // exists
-        if(ump.find(key)!=ump.end())
+        if(addMap.count(key))
         {
-            DListNode* node=remove(ump[key]);
-            node->val=value;
-            insert(node);
+            addMap[key].second=value;
+            makeMostRecent(key);
+            return;
         }
-        else
-        {
-            if(currcap==maxcap)
-            {
-                remove();
-                currcap--;
-            }
-            
-            DListNode* node=new DListNode(key, value);    
-            insert(node);
-            ump[key]=node;
-            currcap++;
-        }
-    }
-    DListNode* remove(DListNode* node)
-    {
-        node->next->prev=node->prev;
-        node->prev->next=node->next;
-        return node;
-    }
-    void remove()
-    {
-        DListNode* node=tail->prev;
-        node->next->prev=node->prev;
-        node->prev->next=node->next;
-        ump.erase(node->key);
-    }
-    void insert(DListNode* node)
-    {
-        node->next=head->next;
-        node->prev=head;
-        head->next=node;
-        node->next->prev=node;
-    }
-    
 
+        if(n==cache.size())
+        {
+            int keyToDel=cache.back();
+            cache.pop_back();
+            addMap.erase(keyToDel);
+        }
+
+        cache.push_front(key);
+
+        addMap[key]={cache.begin(), value};
+    }
 };
 
 /**
